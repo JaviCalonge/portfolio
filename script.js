a9b0c2fc742295a367f0915de7cecf375df01940
Javi Calonge <javicalongem@gmail.com>
2024-10-12 12:06:09 +0200

    Transiciones btn nav y párrafo

--- script.js
+++ script.js
@@ -67,6 +67,14 @@ document
     smoothScrollTo(document.querySelector(".proyectos-titulo"), 2000); // Ajusta el tiempo en milisegundos (2000ms = 2 segundos)
   });
 
+//De nav a sobre mí
+document
+  .querySelector(".nav-btn-about")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    smoothScrollTo(document.querySelector(".about"), 2000); // Ajusta el tiempo en milisegundos (2000ms = 2 segundos)
+  });
+
 //De nav a proyectos
 document
   .querySelector(".nav-btn-work")
