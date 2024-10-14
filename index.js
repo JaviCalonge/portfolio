d9f1c59504cb01e1b59b407b733d19fb429c6517
Javi Calonge <javicalongem@gmail.com>
2024-10-14 09:18:58 +0200

    Añadir transicione .proyectos y nav

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-//SIEMPRE INICA AL PRINCIPIO
+// SIEMPRE INICA AL PRINCIPIO
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
+
+// nav y myWork transicion para aparecer
+window.addEventListener("load", () => {
+  const nav = document.querySelector(".nav");
+  const myWork = document.querySelector(".my-work");
+  myWork.classList.add("visible");
+  setTimeout(() => {
+    nav.classList.add("visible"); // Añade la clase para activar la transición
+  }, 2000);
+});
 
 // EFECTO TYPING PORTADA //
 document.addEventListener("DOMContentLoaded", function () {
@@ -122,3 +132,26 @@ function smoothScrollTo(target, duration) {
 
   requestAnimationFrame(animationScroll);
 }
+
+// EFECTO PARA MOSTRAR LOS PROYECTOS
+
+document.addEventListener("DOMContentLoaded", () => {
+  const proyectos = document.querySelectorAll(".proyecto");
+
+  function checkVisibility() {
+    proyectos.forEach((proyecto) => {
+      const rect = proyecto.getBoundingClientRect();
+      const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+      if (isVisible) {
+        proyecto.classList.add("visible"); // Añade la clase cuando está completamente visible
+      } else {
+        proyecto.classList.remove("visible"); // Elimina la clase cuando sale del viewport
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkVisibility); // Comprueba al hacer scroll
+  window.addEventListener("resize", checkVisibility); // Comprueba al redimensionar la ventana
+  checkVisibility(); // Ejecuta la función al cargar la página
+});
