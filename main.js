44121407ad7aa5685504a278af29d71a319afea2
Javi Calonge <javicalongem@gmail.com>
2024-10-15 18:55:32 +0200

    Scripts

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-import { showAfterTyping } from "./scripts/showAfterTyping";
-import { typing } from "./scripts/typing";
-import { transiciones } from "./scripts/transiciones";
-import { proyectos } from "./scripts/proyectos";
-import { traducciones } from "./scripts/traducciones";
-
-showAfterTyping();
-typing();
-transiciones();
-proyectos();
-traducciones();
-
 // SIEMPRE INICA AL PRINCIPIO
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
